0dd3bc0129b5a7ae6703c88baa107ac2f3277491
udhavrajnath <70680597+udhavrajnath@users.noreply.github.com>
2023-02-19 21:18:00 +1100

    creating page layout

--- src/pages/scripts/Home.js
+++ src/pages/scripts/Home.js
@@ -7,13 +7,40 @@ function Home() {
         <div className='Homepage'>
             <div className='topbar'></div>
             <div className='mainNews'>
-                <div className='left'></div>
+                <div className='left'>
+                  <div className='imageMain'></div>
+                  <div className='lowerText'>
+                    <div className='rightLowerText'></div>
+                    <div className='leftLowerText'></div>
+                  </div>
+                </div>
                 <div className='right'></div>
             </div>
             <div className='topics'>
-                <div className='topicContent'></div>
-                <div className='topicContent'></div>
-                <div className='topicContent'></div>
+                <div className='topicContent'>
+                  <div className='topicImg'></div>
+                  <div className='TopicCntn'>
+                  <div className='TopicTitle'></div>
+                    <div className='TopicDesc'></div>
+                  </div>
+
+                </div>
+                <div className='topicContent'>
+                  <div className='topicImg'></div>
+                  <div className='TopicCntn'>
+                  <div className='TopicTitle'></div>
+                    <div className='TopicDesc'></div>
+                  </div>
+
+                </div>
+                <div className='topicContent'>
+                  <div className='topicImg'></div>
+                  <div className='TopicCntn'>
+                    <div className='TopicTitle'></div>
+                    <div className='TopicDesc'></div>
+                  </div>
+
+                </div>
 
             </div>
         </div>
